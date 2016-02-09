4c700e6aa6f2dc1453d5f09f33cffbde83bc2608
Zhaoxin Sun <sunzhx.js@gmail.com>
2016-02-09 13:49:43 -0600

    ...

--- server.py
+++ server.py
@@ -32,5 +32,5 @@ def get_language_count(lan_id):
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000)) 
-    app.run(host='0.0.0.0',port=port)
+	hostport=int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0',port=hostport)
